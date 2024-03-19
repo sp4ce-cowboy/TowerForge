9c0116cd0160d3a3d7c635be284fdb3e8de666e0
Rubesh <rubesh77@gmail.com>
2024-03-19 18:22:31 +0800

    Add error code to test swiftlint failure in the CI/CD pipeline

--- TowerForge/TowerForgeTests/TowerForgeTests.swift
+++ TowerForge/TowerForgeTests/TowerForgeTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class TowerForgeTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This method is called before the invocation of each test method in the class. Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
