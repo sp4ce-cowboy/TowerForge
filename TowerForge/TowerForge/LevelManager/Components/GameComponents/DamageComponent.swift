62a93f9cc4ba369f45699dac20d5d35ca98ccf1a
zheng-ze <zhengze@u.nus.edu>
2024-03-22 16:51:40 +0800

    Update damage event generation to only occur for entities of different teams

--- TowerForge/TowerForge/LevelManager/Components/GameComponents/DamageComponent.swift
+++ TowerForge/TowerForge/LevelManager/Components/GameComponents/DamageComponent.swift
@@ -32,51 +32,14 @@ class DamageComponent: TFComponent {
         guard canDamage, let entityId = healthComponent.entity?.id else {
             return nil
         }
-        lastAttackTime = CACurrentMediaTime()
-        return DamageEvent(on: entityId, at: lastAttackTime, with: attackPower)
-    }
 
-    override func update(deltaTime: TimeInterval) {
-        super.update(deltaTime: deltaTime)
-
-        // Required components for the current Melee
-        guard let entity = entity,
-              let spriteComponent = entity.component(ofType: SpriteComponent.self) else {
-            return
+        guard let teamA = self.entity?.component(ofType: PlayerComponent.self)?.player,
+              let teamB = healthComponent.entity?.component(ofType: PlayerComponent.self)?.player,
+              teamA != teamB else {
+            return nil
         }
 
-        // TODO: Shift damage logic to damage event and handled by health system.
-        // Loop opposite team's entities
-//        for entity in entityManager.entities {
-//            guard let playerComponent = entity.component(ofType: PlayerComponent.self) else {
-//                return
-//            }
-//            if playerComponent.player == .ownPlayer {
-//                return
-//            }
-//            // Get opposite team's components
-//            guard let oppositeSpriteComponent = entity.component(ofType: SpriteComponent.self),
-//                  let oppositeHealthComponent = entity.component(ofType: HealthComponent.self) else {
-//                return
-//            }
-//
-//            // Check collision with opposite team sprite component
-//            if oppositeSpriteComponent.node
-//                .calculateAccumulatedFrame().intersects(
-//                    spriteComponent.node.calculateAccumulatedFrame()) {
-//
-//                // Check if can attack
-//                if CACurrentMediaTime() - lastAttackTime > attackRate {
-//                    lastAttackTime = CACurrentMediaTime()
-//                    oppositeHealthComponent.decreaseHealth(amount: attackPower)
-//                }
-//
-//            }
-//
-//            // If only used once, then remove from entity
-//            if temporary {
-//                entityManager.removeEntity(with: entity.id)
-//            }
-//        }
+        lastAttackTime = CACurrentMediaTime()
+        return DamageEvent(on: entityId, at: lastAttackTime, with: attackPower)
     }
 }
