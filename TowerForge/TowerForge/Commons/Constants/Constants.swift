16d07de76601ffe7979344dbf741bfd2a3b49af7
Rubesh <rubesh77@gmail.com>
2024-03-25 02:48:06 +0800

    Add Logger functionality

--- TowerForge/TowerForge/Commons/Constants/Constants.swift
+++ TowerForge/TowerForge/Commons/Constants/Constants.swift
@@ -1,14 +1,9 @@
-//
-//  Constants.swift
-//  TowerForge
-//
-//  Created by Rubesh on 25/3/24.
-//
-
 import Foundation
 
 /// A class to encapsulate application wide constants to ensure a single source of truth
 /// across the entire application.
 class Constants {
 
+    /// Universally declaring logging
+    static let LOGGING_IS_ACTIVE = true
 }
