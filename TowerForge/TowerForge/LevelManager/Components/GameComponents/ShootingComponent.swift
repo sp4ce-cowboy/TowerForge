87a4f8b607458094cb15d628fc37961a6e5367cf
zheng-ze <zhengze@u.nus.edu>
2024-03-24 17:10:33 +0800

    Fix bug where shooting entities will attempt to shoot enemy entities behind them.

--- TowerForge/TowerForge/LevelManager/Components/GameComponents/ShootingComponent.swift
+++ TowerForge/TowerForge/LevelManager/Components/GameComponents/ShootingComponent.swift
@@ -37,8 +37,14 @@ class ShootingComponent: TFComponent {
         }
 
         guard let positionA = entityA.component(ofType: PositionComponent.self)?.position,
-              let positionB = entityB.component(ofType: PositionComponent.self)?.position,
-              abs(positionA.x - positionB.x) <= range, abs(positionA.y - positionB.y) <= 50 else {
+              let positionB = entityB.component(ofType: PositionComponent.self)?.position else {
+            return nil
+        }
+
+        let direction = playerA == .ownPlayer ? -1.0 : 1.0
+        let distance = (positionA.x - positionB.x) * direction
+
+        guard distance > 0, distance <= range, abs(positionA.y - positionB.y) <= 50 else {
             return nil
         }
 
