1f605249181c0456de3016d1ba7040c5d86e68a5
Rubesh <rubesh77@gmail.com>
2024-03-25 12:18:34 +0800

    Add unit test for GameEngine

--- TowerForge/TowerForgeTests/GameModuleTests/GameEngineTests.swift
+++ TowerForge/TowerForgeTests/GameModuleTests/GameEngineTests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 final class GameEngineTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func test_initializeGameEngine() {
         let emptyEntityManager = EntityManager()
         let emptySystemManager = SystemManager()
