a4d98a8afad36c22e89e23b88431802a238336cd
MadeEasy <vanessamae23@gmail.com>
2024-03-28 10:36:27 +0800

    Touch up UI for game mode

--- TowerForge/TowerForge/ViewControllers/GameModeViewController.swift
+++ TowerForge/TowerForge/ViewControllers/GameModeViewController.swift
@@ -7,55 +7,26 @@
 import UIKit
 
 class GameModeViewController: UIViewController {
+    @IBOutlet var deathMatchButton: UIButton!
+    @IBOutlet var captureTheFlagButton: UIButton!
     var selectedGameMode = Mode.captureTheFlag
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addButtons()
-    }
-
-    private func addButtons() {
-        let buttonWidth: CGFloat = 200
-        let buttonHeight: CGFloat = 100
-        let _: CGFloat = 20
-
-        // First button: Death Match
-        let deathMatchButton = createButton(title: "Death Match", image: UIImage(named: "Sword")!)
-        deathMatchButton.frame = CGRect(x: (view.frame.width - buttonWidth) / 2, y: 100, width: buttonWidth, height: buttonHeight)
-        deathMatchButton.addTarget(self, action: #selector(deathMatchButtonPressed), for: .touchUpInside)
-        view.addSubview(deathMatchButton)
-
-        // Second button: Capture the Flag
-        let captureTheFlagButton = createButton(title: "Capture the Flag", image: UIImage(named: "Flag")!)
-        captureTheFlagButton.frame = CGRect(x: (view.frame.width - buttonWidth) / 2, y: 250, width: buttonWidth, height: buttonHeight)
-        captureTheFlagButton.addTarget(self, action: #selector(captureTheFlagButtonPressed), for: .touchUpInside)
-        view.addSubview(captureTheFlagButton)
-    }
-
-    private func createButton(title: String, image: UIImage) -> UIButton {
-        let button = UIButton(type: .custom)
-        button.setTitle(title, for: .normal)
-        button.setImage(image, for: .normal)
-        button.setBackgroundImage(UIImage(named: "SquareButton"), for: .normal)
-        return button
-    }
-
-    @objc private func deathMatchButtonPressed() {
-        selectedGameMode = .deathMatch
-        navigateToGameViewController()
-    }
-
-    @objc private func captureTheFlagButtonPressed() {
-        selectedGameMode = .captureTheFlag
-        navigateToGameViewController()
-    }
-
-    private func navigateToGameViewController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil) // Assuming your storyboard name is "Main"
-        guard let gameViewController = storyboard.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else {
-            return
+    @IBAction func deathMatchButtonPressed(_ sender: UIButton) {
+            selectedGameMode = .deathMatch
+            navigateToGameViewController()
         }
-        gameViewController.gameMode = selectedGameMode
-        present(gameViewController, animated: true, completion: nil)
-    }
+
+        @IBAction func captureTheFlagButtonPressed(_ sender: UIButton) {
+            selectedGameMode = .captureTheFlag
+            navigateToGameViewController()
+        }
+
+        private func navigateToGameViewController() {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil) // Assuming your storyboard name is "Main"
+            guard let gameViewController = storyboard.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else {
+                return
+            }
+            gameViewController.gameMode = selectedGameMode
+            present(gameViewController, animated: true, completion: nil)
+        }
 }
