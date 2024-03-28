2cbcf478d4c02256b99c6d49ede40ca9d4417667
MadeEasy <vanessamae23@gmail.com>
2024-03-28 10:36:25 +0800

    Add game state

---
+++ TowerForge/TowerForge/GameModule/GameStates.swift
@@ -0,0 +1,16 @@
+//
+//  GameStates.swift
+//  TowerForge
+//
+//  Created by MacBook Pro on 27/03/24.
+//
+
+import Foundation
+
+enum GameState {
+    case PLAYING
+    case PAUSED
+    case WIN
+    case LOSE
+    case QUIT
+}
