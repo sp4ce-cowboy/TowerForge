ff518db1e47b2ccd166eaccabdcb40d11262e1fb
keith-gan <keith.gan@outlook.com>
2024-03-31 23:33:44 +0800

    Comment out breaking changes

--- TowerForge/TowerForge/Nodes/PowerUpNode.swift
+++ TowerForge/TowerForge/Nodes/PowerUpNode.swift
@@ -34,20 +34,20 @@ class PowerUpNode: TFSpriteNode {
     init(type: PowerUp, delegate: PowerUpNodeDelegate) {
         self.type = type
         self.delegate = delegate
-        super.init(imageName: type.imageName, height: 200.0, width: 140.0)
-
-        self.zPosition = 10.0
-        isUserInteractionEnabled = true
-
-        backgroundNode = SKSpriteNode(color: UIColor.clear, size: self.size)
-        backgroundNode.zPosition = -1
-        addChild(backgroundNode)
-    }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard touches.first != nil else {
-            return
-        }
-        delegate.powerUpNodeDidSelect()
+        super.init(imageName: type.imageName, size: CGSize(width: 200.0, height: 140.0))
+//
+//        self.zPosition = 10.0
+//        isUserInteractionEnabled = true
+//
+//        backgroundNode = SKSpriteNode(color: UIColor.clear, size: self.size)
+//        backgroundNode.zPosition = -1
+//        addChild(backgroundNode)
+//    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard touches.first != nil else {
+//            return
+//        }
+//        delegate.powerUpNodeDidSelect()
     }
 }
