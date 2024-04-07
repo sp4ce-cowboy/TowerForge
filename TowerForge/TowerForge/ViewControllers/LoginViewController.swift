3959d8bdd577db1ce662e067e4d6546e9ec5fd6d
Vanessamae23 <vanessamae23@gmail.com>
2024-04-08 07:51:34 +0800

    Add login view controller

---
+++ TowerForge/TowerForge/ViewControllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  TowerForge
+//
+//  Created by MacBook Pro on 08/04/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailInputField: UITextField!
+    @IBOutlet weak var passwordInputField: UITextField!
+    
+    @IBAction func onLoginPressed(_ sender: Any) {
+    }
+    @IBAction func onRegisterPressed(_ sender: Any) {
+    }
+}
