3a31af7ac5f83afc1b582bc20c5946fb962e3c3f
zheng-ze <zhengze@u.nus.edu>
2024-04-07 17:59:43 +0800

    Add Tests for Events

--- TowerForge/TowerForge/GameModule/Events/EventTransformation.swift
+++ TowerForge/TowerForge/GameModule/Events/EventTransformation.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol EventTransformation: Identifiable {
+protocol EventTransformation: Identifiable, AnyObject {
     var id: UUID { get}
     func transformEvent(event: TFEvent) -> TFEvent
 }
---
+++ TowerForge/TowerForgeTests/EventTests/EventTestsUtils.swift
@@ -0,0 +1,58 @@
+//
+//  EventTestsUtils.swift
+//  TowerForgeTests
+//
+//  Created by Zheng Ze on 7/4/24.
+//
+
+import Foundation
+@testable import TowerForge
+
+class TestEvent: TFEvent {
+    var timestamp = Date().timeIntervalSince1970
+    var entityId = UUID()
+    private(set) var didExecute = false
+
+    func execute(in target: any TowerForge.EventTarget) -> TowerForge.EventOutput {
+        didExecute = true
+        return EventOutput()
+    }
+}
+
+class TestRemoteEvent: TFRemoteEvent {
+    var type = ""
+    var timeStamp = Date().timeIntervalSince1970
+    var source = ""
+    private(set) var didUnpack = false
+
+    func unpack(into eventManager: TowerForge.EventManager, for gamePlayer: TowerForge.UserPlayerId) {
+        didUnpack = true
+        eventManager.add(TestEvent())
+    }
+}
+
+class TestEventTransformationA: EventTransformation {
+    var id = UUID()
+    private(set) var didTransformEvent = false
+
+    func transformEvent(event: any TowerForge.TFEvent) -> any TowerForge.TFEvent {
+        didTransformEvent = true
+        return event
+    }
+}
+
+class TestEventTransformationB: EventTransformation {
+    var id = UUID()
+    private(set) var didTransformEvent = false
+
+    func transformEvent(event: any TowerForge.TFEvent) -> any TowerForge.TFEvent {
+        didTransformEvent = true
+        return event
+    }
+}
+
+class TestEventTarget: EventTarget {
+    func system<T: TFSystem>(ofType type: T.Type) -> T? {
+        TestSystemA() as? T
+    }
+}
