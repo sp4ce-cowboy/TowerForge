5ceb4d0e905e199cdce39189dda4a82799837ce9
zheng-ze <zhengze@u.nus.edu>
2024-04-10 09:59:11 +0800

    Fix CI/CD error

--- TowerForge/TowerForge/GameViewController.swift
+++ TowerForge/TowerForge/GameViewController.swift
@@ -21,7 +21,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.navigationController?.isNavigationBarHidden = true
         AchievementManager.incrementTotalGamesStarted()
         AudioManager.shared.playBackground()
         showGameLevelScene(level: 1) // TODO : Change hardcoded level value
@@ -29,7 +28,6 @@ class GameViewController: UIViewController {
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-//        self.navigationController?.isNavigationBarHidden = false
         AudioManager.shared.pauseBackground()
         gameWorld = nil
     }
