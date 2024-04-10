41cb3949ea36157899f80d1334bbe7b3e18d2b02
Vanessamae23 <vanessamae23@gmail.com>
2024-04-10 12:28:04 +0800

    FInalise leaderboard

--- TowerForge/TowerForge/ViewControllers/LeaderboardViewController.swift
+++ TowerForge/TowerForge/ViewControllers/LeaderboardViewController.swift
@@ -30,7 +30,9 @@ class LeaderboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupScrollView()
         setupViews()
+
         fetchTopRanks(type: self.type.rawValue)
     }
 
@@ -52,12 +54,48 @@ class LeaderboardViewController: UIViewController {
             self.displayLeaderboard(ranks: result)
         }
     }
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+    }
 
     private func displayLeaderboard(ranks: [GameRankData]) {
         for rankData in ranks {
             let rankLabel = UILabel()
+
+            if let customFont = UIFont(name: "Nosifer-Regular", size: 24) {
+                rankLabel.font = customFont
+            }
+
             rankLabel.text = "\(rankData.username): \(rankData.score)"
             rankLabel.textColor = .white
+
+            rankLabel.backgroundColor = .gray
+            rankLabel.layer.cornerRadius = 8
+            rankLabel.layer.masksToBounds = true
+            rankLabel.textAlignment = .center
+            rankLabel.layoutMargins = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+
+            rankLabel.translatesAutoresizingMaskIntoConstraints = false
             stackView.addArrangedSubview(rankLabel)
         }
     }
