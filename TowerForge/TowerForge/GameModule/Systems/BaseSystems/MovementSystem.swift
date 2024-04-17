f17954283b4e1cdeda85507ced9d848dbada3ab7
Rubesh <rubesh77@gmail.com>
2024-04-17 15:23:08 +0800

    Update MoveEvent

--- TowerForge/TowerForge/GameModule/Systems/BaseSystems/MovementSystem.swift
+++ TowerForge/TowerForge/GameModule/Systems/BaseSystems/MovementSystem.swift
@@ -24,8 +24,8 @@ class MovementSystem: TFSystem {
     ///   - displacement: Value of the required displacement
     func handleMovement(for entityId: UUID, with displacement: CGVector) {
         /*guard isActive else { TODO: Implement boolean check
-            return
-        }*/
+         return
+         }*/
         guard let currentEntity = entityManager.entity(with: entityId),
               let movementComponent = currentEntity.component(ofType: MovableComponent.self) else {
             return
@@ -35,25 +35,32 @@ class MovementSystem: TFSystem {
     }
 
     private func processMovableComponent(_ movableComponent: MovableComponent, time: CGFloat) {
-        guard movableComponent.shouldMove, let entity = movableComponent.entity,
-              let player = entity.component(ofType: PlayerComponent.self)?.player else {
-            return
-        }
+        /*
+         guard movableComponent.shouldMove, let entity = movableComponent.entity,
+         let player = entity.component(ofType: PlayerComponent.self)?.player else {
+         return
+         }
+         
+         if entity is BaseProjectile {
+         movableComponent.update(deltaTime: time)
+         return
+         }
+         
+         guard eventManager.isHost else {
+         return
+         }
+         
+         let displacement = movableComponent.velocity * player.getDirectionVelocity() * time
+         guard let player = eventManager.currentPlayer else {
+         eventManager.add(MoveEvent(on: entity.id, at: Date().timeIntervalSince1970, with: displacement))
+         return
+         }
+         eventManager.add(RemoteMoveEvent(id: entity.id, moveBy: displacement, gamePlayer: player))
+         */
 
-        if entity is BaseProjectile {
-            movableComponent.update(deltaTime: time)
+        guard movableComponent.shouldMove else {
             return
         }
-
-        guard eventManager.isHost else {
-            return
-        }
-
-        let displacement = movableComponent.velocity * player.getDirectionVelocity() * time
-        guard let player = eventManager.currentPlayer else {
-            eventManager.add(MoveEvent(on: entity.id, at: Date().timeIntervalSince1970, with: displacement))
-            return
-        }
-        eventManager.add(RemoteMoveEvent(id: entity.id, moveBy: displacement, gamePlayer: player))
+        movableComponent.update(deltaTime: time)
     }
 }
